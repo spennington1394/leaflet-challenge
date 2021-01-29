2398f8177234d5fa1deed1386be2a77d08584372
spennington <spennington13@gmail.com>
2021-01-29 14:22:37 -0500

    update
    some cleaning and trouble shooting. changed the opacity and fillOpacity so that dots displayed better.  Had several instances where variable was not carried properly. lastly created myMap variable

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,26 +10,45 @@ function createMap(earthquake){
   //loop to create markers
   //start with coordinates
   earthquakeMarkers = earthquake.map((feature) =>
-  L.cirleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],{
+  L.circleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],{
     //set marker radius to magnitude of earthquake and create pop-up with mag info
     //location and time
-    radius: mag(feature.properties.mag),
+    radius: (feature.properties.mag),
     stroke: true,
     color: 'green',
-    opacity: .5,
+    opacity: 5,
     weight: 1,
     fill: true,
-    fillcolor: magColor(feature.properties.mag),
-    fillOpacity: .5
+    fillcolor: (feature.properties.mag),
+    fillOpacity: 5
   })
   .bindPopup("<h1> Magnitude: " + feature.properties.mag + feature.properties.place +
   new Date(feature.properties.time))
   )
-  //
+  //earthquake layer to marker cluster group
   var earthquakes = L.layerGroup(earthquakeMarkers)
-  console.log(d3.extent(d3.values(earthquake),((d) => +d.properties.mag));
-  var magnitude = earthquake.map((d) => magCheck(+d.properties.mag));
-  
+  console.log(d3.extent(d3.values(earthquake),((d) => +d.properties.mag)));
+  var magnitude = earthquake.map((d) => (+d.properties.mag));
+  console.log(d3.extent(magnitude));
+  console.log(magnitude);
+
+  //define streetmap (copy from GeoJSON in activities 1)
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Â© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> Â© <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+  //create map with streetmap and earthquake layers
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
 }
 
 
