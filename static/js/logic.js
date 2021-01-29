f5a6b9181c187159f96badf47f0a2ef9d89037c6
spennington <spennington13@gmail.com>
2021-01-29 13:38:50 -0500

    Update logic.js
    query_url var containing api url and d3.json to get data from query_url

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,11 @@
+//store api url inside variable (looking at week summary)
+var query_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Get requet with query_url and send to data.features
+d3.json(query_url,function(data){
+  createMap(data.features);
+});
+
 
 
 
