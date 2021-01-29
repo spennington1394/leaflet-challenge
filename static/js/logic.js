739665777b0ecce6b4a4c64c4cd75c33b180a4f2
spennington <spennington13@gmail.com>
2021-01-29 13:48:56 -0500

    update logic.js
    working on createMap function and creating markers and popup information

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,7 +6,25 @@ d3.json(query_url,function(data){
   createMap(data.features);
 });
 
-
+function createMap(earthquake){
+  //loop to create markers
+  //start with coordinates
+  earthquakeMarkers = earthquake.map((feature) =>
+  L.cirleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],{
+    //set marker radius to magnitude of earthquake and create pop-up with mag info
+    //location and time
+    radius: mag(feature.properties.mag),
+    stroke: true,
+    color: 'green',
+    opacity: .5,
+    weight: 1,
+    fill: true,
+    fillcolor: magColor(feature.properties.mag),
+    fillOpacity: .5
+  })
+  .bindPopup("<h1> Magnitude: " + feature.properties.mag +)
+  )
+}
 
 
 
